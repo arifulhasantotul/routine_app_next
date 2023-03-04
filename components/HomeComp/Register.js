049ca237a59865c0d62f7703659b3faa356b2886
arifulhasantotul <mdahtotul@gmail.com>
2023-03-04 23:30:20 +0600

    create account responsive frontend done with validation

---
+++ components/HomeComp/Register.js
@@ -0,0 +1,248 @@
+import styles from "@/styles/Register.module.css";
+import countryData from "@/utils/countriesData.json";
+import { useState } from "react";
+import RoundedFormButton from "../FormButton/RoundedFormButton";
+
+const Register = () => {
+  const initialData = {
+    name: "",
+    email: "",
+    institution: "",
+    batch: "",
+    country: "",
+    dialCode: "",
+    phone: "",
+    numLength: 0,
+  };
+
+  const [formData, setFormData] = useState(initialData);
+  const [errFormData, setErrFormData] = useState(initialData);
+
+  const batches = [
+    { id: 1, title: "JSC" },
+    { id: 2, title: "SSC" },
+    { id: 3, title: "HSC" },
+  ];
+
+  const checkValidation = (e) => {
+    // warning sign: &#9888;
+    const { name, value } = e.target;
+    if (name === "name") {
+      if (!value) {
+        setErrFormData((prev) => ({
+          ...prev,
+          [name]: "Name required!",
+        }));
+      } else if (value.length < 3) {
+        setErrFormData((prev) => ({ ...prev, [name]: "Name is too short!" }));
+      } else {
+        setErrFormData((prev) => ({ ...prev, [name]: "" }));
+      }
+    } else if (name === "email") {
+      if (!value) {
+        setErrFormData((prev) => ({ ...prev, [name]: "Email required!" }));
+      } else if (!value.includes("@")) {
+        setErrFormData((prev) => ({ ...prev, [name]: "Invalid email!" }));
+      } else {
+        setErrFormData((prev) => ({ ...prev, [name]: "" }));
+      }
+    } else if (name === "phone") {
+      if (!value) {
+        setErrFormData((prev) => ({
+          ...prev,
+          [name]: `Phone number required!`,
+        }));
+      }
+      if (value.length !== formData.numLength) {
+        setErrFormData((prev) => ({
+          ...prev,
+          [name]: `Phone number must be ${formData.numLength} digits`,
+        }));
+      } else {
+        setErrFormData((prev) => ({ ...prev, [name]: "" }));
+      }
+    } else if (name === "country") {
+      if (!value) {
+        setErrFormData((prev) => ({
+          ...prev,
+          [name]: "Select a country!",
+        }));
+      } else {
+        setErrFormData((prev) => ({ ...prev, [name]: "" }));
+      }
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "country") {
+      if (!value) {
+        setFormData((prev) => ({
+          ...prev,
+          dialCode: "",
+          country: "",
+          numLength: 0,
+        }));
+        return;
+      }
+      let countryName = value.split("+")[0];
+      let countryDialCode = value.split("+")[1];
+      setFormData((prev) => ({
+        ...prev,
+        dialCode: "+" + countryDialCode.split("*")[0],
+        country: countryName,
+        numLength: countryDialCode.split("*")[1],
+      }));
+    } else {
+      setFormData((prev) => ({ ...prev, [name]: value }));
+    }
+  };
+
+  const handleReset = () => {
+    setFormData(initialData);
+    setErrFormData(initialData);
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("institution").value = "";
+    document.getElementById("batch").value = "";
+    document.getElementById("country").value = "";
+    document.getElementById("phone").value = "";
+  };
+
+  return (
+    <>
+      <div className={styles.form_wrapper}>
+        <h2>Create Account</h2>
+        <form className={styles.form}>
+          <div className={styles.form_group}>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className={styles.input}
+              value={formData?.name}
+              placeholder="e.g. MD ARIFUL HASAN"
+              onChange={handleChange}
+              onBlur={checkValidation}
+              required
+            />
+          </div>
+          {errFormData.name && (
+            <div className={styles.error}> &#9888; {errFormData.name}</div>
+          )}
+
+          <div className={styles.form_group}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              className={styles.input}
+              value={formData?.email}
+              placeholder="e.g. mdahtotul@gmail.com"
+              onChange={handleChange}
+              onBlur={checkValidation}
+              required
+            />
+          </div>
+          {errFormData.email && (
+            <div className={styles.error}> &#9888; {errFormData.email}</div>
+          )}
+          <div className={styles.form_group}>
+            <label htmlFor="institution">Institution</label>
+            <input
+              type="text"
+              name="institution"
+              id="institution"
+              className={styles.input}
+              value={formData?.institution}
+              placeholder="e.g. BAF SHAHEEN SCHOOL & COLLEGE"
+              onChange={handleChange}
+              onBlur={checkValidation}
+              required
+            />
+          </div>
+          <div className={styles.form_group}>
+            <label htmlFor="batch">Batch</label>
+            <select
+              name="batch"
+              id="batch"
+              value={formData?.batch}
+              onChange={handleChange}
+              required
+            >
+              <option value="">--Select Batch--</option>
+              {batches.map((batch) => (
+                <option key={batch.id} value={batch.title}>
+                  {batch.title}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className={styles.form_group}>
+            <label htmlFor="country">Country</label>
+            <select
+              name="country"
+              id="country"
+              onChange={handleChange}
+              required
+            >
+              <option value="">--Select Country--</option>
+              {countryData.map((item, idx) => (
+                <option
+                  key={idx}
+                  value={`${item.name.common}${
+                    item.idd.root + item.idd.suffixes[0] + "*" + item.uniqueLen
+                  }`}
+                >
+                  {item.name.common} ({item.idd.root + item.idd.suffixes[0]})
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className={styles.form_group}>
+            <label htmlFor="batch">Phone:</label>
+            <span className={`${styles.input} ${styles.number}`}>
+              {formData?.dialCode}
+              <input
+                type="number"
+                name="phone"
+                id="phone"
+                className="hide_numbers_arrow"
+                placeholder="+880 xxxxxxxxx"
+                onChange={handleChange}
+                onBlur={checkValidation}
+                disabled={!formData?.dialCode}
+                required
+              />
+            </span>
+          </div>
+          {errFormData.phone && (
+            <div className={styles.error}> &#9888; {errFormData.phone}</div>
+          )}
+
+          <div className={styles.btn_group}>
+            <RoundedFormButton
+              type="button"
+              value="Reset"
+              color="#ed4141"
+              bgColor="#fff"
+              onClick={handleReset}
+            />
+            <RoundedFormButton
+              type="submit"
+              value="Sign Up"
+              color="#29fd53"
+              bgColor="#fff"
+            />
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
